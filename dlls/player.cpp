be10125377e1d477dbaae5b43d0cb310823bb491
Bacontsu <62763382+bacontsu@users.noreply.github.com>
2023-07-02 20:33:29 +0700

    Touch up sliding speed

--- dlls/player.cpp
+++ dlls/player.cpp
@@ -2191,7 +2191,7 @@ void CBasePlayer::PreThink()
 	}
 
 	// bacontsu - sliding logic
-	float SlidingTime = 1.5f;
+	float SlidingTime = 1.25f;
 	int SlidingCounter = (int)(SlidingTime / 0.05f);
 	if (pev->velocity.Length2D() > 350.0f && (pev->button & IN_DUCK) && m_iSlidingStage == 0 && m_flSlidingCooldown < gpGlobals->time)
 	{
